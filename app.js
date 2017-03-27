044de15bf868f4b90d74b900a87648daa316219c
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-03-26 21:05:58 -0500

    Added node module, mongo db and changing index.js now

--- app.js
+++ app.js
@@ -5,40 +5,62 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
 var index = require('./routes/index');
 
 var app = express();
 
+var mongo_pw = process.env.Mongo_PW;
+var url = 'mongodb://localhost:27017/Travel';
+MongoClient.connect(url, function (err, db)
+{
+    assert.equal(null, err);
+    console.log('connected to MongoDB');
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+    // make db available to routes
+// add app.use() call to add the db to each request
 
-app.use('/', index);
+// This function will run before each route handler…
+
+
+    app.use('/', function (req, res, next)
+    {
+        req.db = db;
+        next();
+
+    });
+
+    app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    app.use(function (err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
+}); // end of MongoDb client
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,89 +4,104 @@ var router = express.Router();
 
 // "Database". Names of places, and whether the user has visited it or not.
 
-var places = [
-{id: "1", name: "Rome", visited: true},
-{id: "2", name: "New York", visited: false},
-{id: "3", name: "Tokyo", visited: false}
-];
-var counter = places.length;
+
+
 
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Travel Wish List', places : places });
-});
+router.get('/', function(req, res, next)
+{
+    req.db.collection('places').find().toArray(function (err, placesDocs) {
+        console.log(placesDocs);
+        if (err) {
+            return next(err)
+        }
+        else {
+            return res.render('index', {title: 'Travel Wish List', places: placesDocs});
+        }
+    });
 
+});
 
 /* GET all items home page. */
-router.get('/all', function(req, res) {
-  res.json(places);
+router.get('/all', function(req, res)
+{
+    req.db.collection('places').find().toArray(function (err, placesDocs) {
+        console.log(placesDocs);
+        res.json(placesDocs);
+    });
 });
-
-
+//
+//
 /* POST - add a new location */
-router.post('/add', function(req, res) {
+router.post('/add', function(req, res, next)
+{
+   var counter= req.db.collection('places').find().count();
+    console.log("I am counter " + counter);
+    req.db.collection('places').find({'name':req.body.name}).toArray( function (err, doc)
+    {
+        console.log(req.body.name);
+        console.log(doc.length);
+        // gives the number of duplicate entry
+        //checks if the list for the flower name is 0;then add it else give an error message
+        if(doc.length == 0)
+        {
 
-  var name = req.body.name;
-  var place = { 'id': ++counter + "" , 'name': name, 'visited': false };
+            req.db.collection('places').insertOne({'id':++counter,'name':req.body.name,'visited':false }, function (err)
+            {
+                console.log("Counter Increased " + counter);
+                if (err) {
+                    return next(err);
+                }
+                return res.redirect('/'); // directs to home-page
+            });
+        }
+        else {
+            res.send("Duplicate Entry");
 
-  places.push(place);
+        }
 
-  console.log('After POST, the places list is');
-  console.log(places);
-
-  res.status(201);      // Created
-  res.json(place);      // Send new object data back as JSON, if needed.
-
-  // TODO may want to check if place already in list and don't add.
-
-});
-
-
-/* PUT - update whether a place has been visited or not */
-router.put('/update', function(req, res){
-
-  var id = req.body.id;
-  var visited = req.body.visited == "true";  // all the body parameters are strings
-
-  for (var i = 0 ; i < places.length ; i++) {
-    var place = places[i];
-    if (place.id == id) {
-      place.visited = visited;
-      places[i] = place;
-    }
-  }
-
-  console.log('After PUT, the places list is');
-  console.log(places);
-
-  res.json(place);
+    });
 
 });
-
-
-router.delete('/delete', function(req, res){
-
+router.post('/delete', function(req, res, next)
+{
+  console.log("I am body" + req.body);
   var place_id = req.body.id;
-  console.log(place_id);
+  console.log("I am the id"+place_id);
+  req.db.collection('places').deleteOne({'id':place_id},function (err, docs)
+    {
+        if (err)
+        {
+            return next(err);
+        }
+        return res.render('delete_places.hbs',{'places': req.body.name} ); // directs to delete-page
+    });
 
-  for (var i = 0 ; i < places.length ; i++) {
-    var place = places[i];
-    if (place.id == place_id) {
-      places.splice(i, 1);  //Delete the element at this position
-      res.json(place);
-      break;
-    }
-  }
-
-  console.log('After DELETE, the places list is');
-  console.log(places);
-
-  res.status(200);
-  res.end();
 
 });
 
-
+// /* PUT - update whether a place has been visited or not */
+// router.put('/update', function(req, res){
+//
+//   var id = req.body.id;
+//   var visited = req.body.visited == "true";  // all the body parameters are strings
+//
+//   for (var i = 0 ; i < places.length ; i++) {
+//     var place = places[i];
+//     if (place.id == id) {
+//       place.visited = visited;
+//       places[i] = place;
+//     }
+//   }
+//
+//   console.log('After PUT, the places list is');
+//   console.log(places);
+//
+//   res.json(place);
+//
+// });
+//
+//
 
 module.exports = router;
